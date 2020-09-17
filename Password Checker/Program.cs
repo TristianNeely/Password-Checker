218805843be939cc9ca8687dce54b612f3b0c98f
Tristian Neely <Neely_Tristian@student.mahoningctc.com>
2020-09-17 10:17:53 -0400

    added color

--- Password Checker/Program.cs
+++ Password Checker/Program.cs
@@ -4,6 +4,8 @@ namespace PasswordCheckerCodeCademyProject
 {
     class Program
     {
+
+        static Random randomObject = new Random();
         static void Main(string[] args)
         {
 
@@ -14,19 +16,22 @@ namespace PasswordCheckerCodeCademyProject
             string specialChars = "#*%$&(#@-";
 
             int score = 0;
-
+            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
             Console.WriteLine("Please Enter a password to determine it's strength.");
             string password = Console.ReadLine();
+            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
             Console.WriteLine($"You entered the password: {password}. ");
 
             if (password.Length >= minLength)
             {
+                Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                 score++;
                 Console.WriteLine($"Nice job on length. You get a point. Your Score is = {score}");
              
                 
                 if (Tools.Contains(password, uppercase))
                 {
+                    Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                     ++score;
                     Console.WriteLine($"Nice job on using UPPERCASE. You get a point. Your Score is = {score}");
 
@@ -34,18 +39,21 @@ namespace PasswordCheckerCodeCademyProject
 
                 if (Tools.Contains(password, lowercase))
                 {
+                    Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                     ++score;
                     Console.WriteLine($"Nice job on using lowercase. You get a point. Your Score is = {score}");
 
                 }
                 if (Tools.Contains(password, digits))
                 {
+                    Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                     ++score;
                     Console.WriteLine($"Nice job on using digits. You get a point. Your Score is = {score}");
 
                 }
                 if (Tools.Contains(password, specialChars))
                 {
+                    Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                     ++score;
                     Console.WriteLine($"Nice job on using Special Characters. You get a point. Your Score is = {score}");
                     Console.WriteLine(score);
@@ -55,21 +63,27 @@ namespace PasswordCheckerCodeCademyProject
                 switch (score)
                 {
                     case 5:
+                        Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                         Console.Write("The password is extremely strong");
                         break;
                     case 4:
+                        Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                         Console.Write("The password is extremely strong");
                         break;
                     case 3:
+                        Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                         Console.WriteLine("The password is strong");
                         break;
                     case 2:
+                        Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                         Console.WriteLine("The password is medium");
                         break;
                     case 1:
+                        Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                         Console.WriteLine("The password is weak");
                         break;
                     default:
+                        Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                         Console.WriteLine("Hmm.. Im not sure");
                         break;
 
